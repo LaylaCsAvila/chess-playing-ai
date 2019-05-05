92e4a2d09b4c7780fdbb717e82b3fd1d102bb0e9
Layla <laylacsa@msn.com>
2019-05-05 18:41:47 -0300

    Todas as integrações concluídas, IA se movendo aleatoriamente

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,51 @@
-var config = {
-  position: 'start',
-  draggable: true
-}
+// definindo um objeto de tabuleiro e um de partida para interagir com
+const partida = new Chess();
+let tabuleiro;
 
-var board1 = ChessBoard('board1', config);
+// impede que o computador jogue se:
+// não for a vez das brancas
+// o jogo acabou
+const movimentoComeca = function(source, piece, position, orientation) {
+  if (partida.in_checkmate() === true || partida.in_draw() === true ||
+    piece.search(/^b/) !== -1) {
+    return false;
+  }
+};
+
+// aqui fica a
+const fazMovimento = function() {
+  var possibleMoves = partida.moves();
+
+  // partida over
+  if (possibleMoves.length === 0) return;
+
+  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  partida.move(possibleMoves[randomIndex]);
+  tabuleiro.position(partida.fen());
+};
+
+const aoLargar = function(origem, destino) {
+  //  movimento realizado é legal? Se não for, desfaz
+  var move = partida.move({
+    from: origem,
+    to: destino,
+    promotion: 'q'
+  });
+  if (move === null) return 'snapback';
+
+  // chama a IA para jogar no turno das peças pretas
+  window.setTimeout(fazMovimento, 250);
+};
+
+// atualiza o tabuleiro depois da jogada
+const movimentoTermina = function() { tabuleiro.position(partida.fen()); };
+
+// configurações constantes e retorno do tabuleiro modificável
+const config = {
+  position: 'start',
+  draggable: true,
+  onDragStart: movimentoComeca,
+  onDrop: aoLargar,
+  onSnapEnd: movimentoTermina,
+}
+tabuleiro = ChessBoard('tabuleiro', config);
